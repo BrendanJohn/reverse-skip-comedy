947dba39612972cf4e64e52169bb68196dcb6b2b
Brendan M <bjohnmurphy1984@gmail.com>
2023-12-23 11:17:46 -0500

    main logo

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,11 +1,22 @@
 import Image from 'react-bootstrap/Image';
+import './../styles/styles.css'
 
 const Gallery = () => {
     return (
         <div className='wrapper'>
             <div className="text-center">
-            <h3>Gallery</h3>
-                <Image src="./images/reverseSkip.jpg" fluid />
+                <Image className="gallery-image" src="./images/gallery/8M1A8016.jpg" fluid />
+                < br/>
+                <Image className="gallery-image" src="./images/gallery/8M1A8022.jpg" fluid />
+                < br/>
+                <Image className="gallery-image" src="./images/gallery/8M1A8108.jpg" fluid />
+                < br/>
+                <Image className="gallery-image" src="./images/gallery/8M1A8179.jpg" fluid />
+                < br/>
+                <Image className="gallery-image" src="./images/gallery/8M1A8224.jpg" fluid />
+                < br/>
+                <Image className="gallery-image" src="./images/gallery/8M1A8229.jpg" fluid />
+                < br/>
             </div>
         </div>
           );
