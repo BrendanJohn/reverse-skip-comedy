9edca71006c549783a3d7acdcfe7853d7c868565
Brendan M <bjohnmurphy1984@gmail.com>
2024-01-07 10:28:25 -0500

    updated about

--- src/pages/Social.js
+++ src/pages/Social.js
@@ -6,15 +6,7 @@ const Social = () => {
         <div className="container">
         <div className='wrapper'>
             <div className="text-center about-text">
-            <h2>Underground, unique, quirky, old school, cool and understated</h2>
-                <div className="text-center">
-                    <p>Quentin James and Dennis Hurley met in the Boston comedy scene in the late 2000s. 
-                        They were part of the New England based sketch comedy troupe, Friends of Gertrude. 
-                        Their work has been featured at the LA Comedy Festival at ImprovOlympic, Boston Comedy Festival, 
-                        Philly Sketch Fest, NBC, Comedy Central, Inside Edition, CNN & MSNBC.
-                    </p>
-                </div>
-                <Image className="gallery-image" src="./images/DennisAndQuentinLogo.jpg" fluid />
+                <Image className="gallery-image" src="./images/gallery/flower.jpg" fluid />
                 < br/>
             </div>
         </div>
