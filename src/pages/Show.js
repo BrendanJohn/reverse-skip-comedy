759a8c15c7f57d913e35d766f7c4c037a9a040cc
Brendan M <bjohnmurphy1984@gmail.com>
2024-01-07 10:17:32 -0500

    updated about

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,10 +1,14 @@
 import Image from 'react-bootstrap/Image';
 const Show = () => {
   return (
+    <div className="container">
     <div className='wrapper'>
-        <div className="text-center">
+        <div className="text-center about-text">
         <Image src="./images/RedTie.jpg" fluid />
+        < br/>
+        <p>"Kuru/Speak Like a Child" written by Jaco Pastorius & Herbie Hancock</p>
         </div>
+    </div>
     </div>
       );
     };
