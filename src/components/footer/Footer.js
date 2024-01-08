d367f45a91dc8fa6931e35a030181af3696eaf6e
Brendan M <bjohnmurphy1984@gmail.com>
2024-01-08 10:21:01 -0500

    updated about

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -13,7 +13,7 @@ const Footer = ({ subtitle }) => (
     </div>
     <div className="col-10 d-flex justify-content-center" id="socials">
           <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://www.youtube.com/denhurley" />
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://www.vimeo.com/user582835" />
+          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://www.vimeo.com/user206540589" />
           <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="https://twitter.com/itsdendo" />
           <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://dennishurley.tumblr.com/" />
           <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://instagram.com/dendohurley" />
