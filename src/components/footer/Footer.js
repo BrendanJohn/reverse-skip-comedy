6e63f48aad109f8a39ffc1090ef4b61829e924eb
Brendan M <bjohnmurphy1984@gmail.com>
2024-01-08 12:07:56 -0500

    updated about

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,12 +12,10 @@ const Footer = ({ subtitle }) => (
     <div className="col">
     </div>
     <div className="col-10 d-flex justify-content-center" id="socials">
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://www.youtube.com/denhurley" />
+          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://www.youtube.com/reverseskipcomedy" />
           <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://www.vimeo.com/user206540589" />
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="https://twitter.com/itsdendo" />
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://dennishurley.tumblr.com/" />
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://instagram.com/dendohurley" />
-          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://www.facebook.com/dendocomedy" />
+          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="https://twitter.com/ReverseSkipDQ" />
+          <SocialIcon style={{ height: 38, width: 38 }} className ="social-icon" url="http://www.facebook.com/reverseskipcomedy" />
     </div>
     <div className="col">
     </div>
